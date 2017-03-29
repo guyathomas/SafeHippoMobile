e96dbf5ca559aacef571f039ae5a34276704bcd8
Guy <guythomas721@gmail.com>
2017-03-29 16:10:23 +1100

    Origin field now updates correctly

--- App/Components/LocationField.js
+++ App/Components/LocationField.js
@@ -8,9 +8,6 @@ import {
 var {GooglePlacesAutocomplete} = require('react-native-google-places-autocomplete');
 import env from '../../env.js'
  
-const homePlace = {description: 'Home', geometry: { location: { lat: 48.8152937, lng: 2.4597668 } }};
-const workPlace = {description: 'Work', geometry: { location: { lat: 48.8496818, lng: 2.2940881 } }};
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -31,8 +28,8 @@ var LocationField = React.createClass({
         autoFocus={false}
         fetchDetails={true}
         onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
-          console.log(data);
-          console.log(details);
+          console.log('data', data);
+          console.log('details', details);
         }}
         getDefaultValue={() => {
           return ''; // text input default value
@@ -41,7 +38,7 @@ var LocationField = React.createClass({
           // available options: https://developers.google.com/places/web-service/autocomplete
           key: env.GOOGLE_KEY,
           language: 'en', // language of the results
-          types: '(cities)', // default: 'geocode'
+          types: 'geocode', // default: 'geocode'
         }}
         styles={{
           textInputContainer: {
@@ -59,26 +56,19 @@ var LocationField = React.createClass({
             color: '#5d5d5d',
             fontSize: 16
           },
+          listView: {
+            backgroundColor: 'white',
+            marginTop: 0
+          }
         }}
         
         currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
         currentLocationLabel="Current location"
-        nearbyPlacesAPI='GooglePlacesSearch' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
+        nearbyPlacesAPI='GoogleReverseGeocoding' // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
         GoogleReverseGeocodingQuery={{
+          'address': '944 Market Street'
           // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
         }}
-        GooglePlacesSearchQuery={{
-          // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-          rankby: 'distance',
-          types: 'food',
-        }}
-        
-        
-        filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-        
-        predefinedPlaces={[homePlace, workPlace]}
-        
-        predefinedPlacesAlwaysVisible={true}
       />
     );
   }
